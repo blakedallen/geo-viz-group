199326e511fcdbc0786fce33cdae9b963b55ef4c
blakedallen <blakedallen@gmail.com>
2020-11-25 12:56:16 -0800

    update local server and add preliminary model

--- local_server.py
+++ local_server.py
@@ -4,9 +4,14 @@ from flask import request
 from flask import Response
 from flask import render_template
 
+from model import gen_slr
+
 app = Flask(__name__, 
         static_folder="", 
         static_url_path="")
+
+
+
 
 @app.route('/')
 def hello_world():
@@ -14,8 +19,9 @@ def hello_world():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-	content = request.get_json()
-	return json.dumps(content)
+    content = request.get_json()
+
+    return json.dumps(content)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- run_server.sh
+++
@@ -1,3 +0,0 @@
-
-export FLASK_APP=local_server.py
-python3 -m flask run
