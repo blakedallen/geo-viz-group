331037706da67f6bbebc4d7025aa8d7b3b35b45e
Yang Jing <50113424+lateflare@users.noreply.github.com>
2020-12-06 08:59:00 -0800

    added a quick intro page

--- app.py
+++ app.py
@@ -3,23 +3,25 @@ from flask import Flask
 from flask import request
 from flask import Response
 from flask import render_template
-
 from model import gen_slr
 
 application = Flask(__name__, 
         template_folder="templates",
-        static_folder="", 
+        static_folder="",
         static_url_path="")
-
 
 @application.route('/')
 @application.route('/index')
 def hello_world():
-    return render_template('seattle.html')
+    return render_template('home.html')
 
 @application.route("/chart2")
 def chart2():
     return render_template("multiline_chart.html")
+
+@application.route("/seattle")
+def sea_map():
+    return render_template("seattle.html")
 
 @application.route("/predict", methods=["POST"])
 def predict():
@@ -27,17 +29,18 @@ def predict():
     try:
         years = int(p["years"])
         gt = int(p["gt"])
-    except Execption as e:
-        return json.dumps({"sea_level":0})
+    except Exception as e:
+        return json.dumps({
+            "sea_level":0, 
+            "error":str(e),
+            "r":p})
     res = gen_slr(years, gt)
-    
     #Note: max sea level rise set here
     #https://www.usgs.gov/faqs/how-would-sea-level-change-if-all-glaciers-melted?qt-news_science_products=0#qt-news_science_products
     if res > 70:
         res = 70
 
-    return json.dumps({"sea_level":res})
+    return json.dumps({"sea_level":res, "r":p})
 
 if __name__ == "__main__":
     application.run(debug=True)
-
